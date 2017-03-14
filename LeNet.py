fixed softmax bug

LeNet.py
@@ -1,4 +1,6 @@
 import numpy as np
+from tsne import bh_sne
+from matplotlib.pyplot import scatter, show
 import time
 import matplotlib.pyplot as plt
 from conv import Conv
@@ -7,10 +9,11 @@ from softmax import Softmax
 import os
 from tqdm import tqdm
 from pool import MaxPool_2
+from itertools import product
 
 
 class LeNet(object):
-    def __init__(self, activation='relu'):
+    def __init__(self, activation='tanh'):
         self.activation = activation
         self.no_of_classes = 10
         self.build_model()
@@ -28,9 +31,9 @@ class LeNet(object):
         self.conv_layers += [Conv(6, 16, 5, self.activation)]
         # 16x8x8  -> 16x4x4
         self.conv_layers += [MaxPool_2()]
-        # 16x4x4  -> 120x1x1
-        self.conv_layers += [Conv(16, 120, 4, self.activation)]
 
+        # 256 -> 120
+        self.linear_layers += [Linear(16*4*4, 120, self.activation)]
         # 120 -> 84
         self.linear_layers += [Linear(120, 84, self.activation)]
         # 84  -> 10
@@ -45,7 +48,7 @@ class LeNet(object):
         for l in self.conv_layers:
             out = l.forward(out)
 
-        out = out.reshape(120)
+        out = out.reshape(16*4*4)
         for l in self.linear_layers:
             out = l.forward(out)
 
@@ -74,11 +77,95 @@ class LeNet(object):
         for l in self.linear_layers[::-1]:
             dE_dX = l.backward(dE_dX)
 
-        dE_dX = dE_dX.reshape(120, 1, 1)
+        dE_dX = dE_dX.reshape(16, 4, 4)
         for l in self.conv_layers[::-1]:
             dE_dX = l.backward(dE_dX)
 
         return dE_dX
+
+    def tell_time_taken(self):
+        inp_tensor = np.ndarray((1, 28, 28))
+        self.forward(inp_tensor)
+        c = 1
+        l = 1
+        for conv in self.conv_layers:
+            print 'Time taken for conv' + str(c) + ' = ' + str(conv.time_taken)
+            c += 1
+
+        for lin in self.linear_layers:
+            print 'Time taken for fc' + str(l) + ' = ' + str(lin.time_taken)
+            l += 1
+
+    def get_numeric_grads(self, target=4):
+        inp_tensor = np.random.normal(loc=2.0,
+                                      scale=0.1,
+                                      size=(1, 28, 28))
+        self.forward(inp_tensor)
+        self.backward(target)
+        epsilon = 1e-10
+
+        def get_conv_grad():
+            c1 = self.conv_layers[0]
+            W, b = self.conv_layers[0].params
+            in_chs, out_chs, kr_w, kr_h = W.shape
+            calculate_W_grad = np.ndarray(W.shape)
+            squaredW = np.ndarray(W.shape)
+            for in_ch in xrange(in_chs):
+                for out_ch in xrange(out_chs):
+                    for i, j in product(xrange(kr_w), repeat=2):
+                        orig_value = W[in_ch][out_ch][i][j]
+                        self.conv_layers[0].params[0][in_ch][out_ch][i][j] \
+                            += epsilon
+                        self.forward(inp_tensor)
+                        E_plus = self.cross_entropy_loss(target)
+                        self.conv_layers[0].params[0][in_ch][out_ch][i][j] \
+                            -= 2*epsilon
+                        self.forward(inp_tensor)
+                        E_minus = self.cross_entropy_loss(target)
+                        grad_val = (E_plus - E_minus)/(epsilon*2)
+                        calculate_W_grad[in_ch][out_ch][i][j] = grad_val
+                        squaredW[in_ch][out_ch][i][j] \
+                            = (grad_val - c1.gradParams[0][in_ch][out_ch][i][j])**2
+                        self.conv_layers[0].params[0][in_ch][out_ch][i][j] \
+                            = orig_value
+
+                        print grad_val
+                        print orig_value
+                        if not orig_value == 0.0:
+                            print abs(grad_val - orig_value)/abs(orig_value)
+                        print '--------'
+
+        def get_lin_grad(layer=0):
+            l1 = self.linear_layers[layer]
+            W, b = self.linear_layers[layer].params
+            inp_dim, out_dim = W.shape
+            calculate_W_grad = np.ndarray(W.shape)
+            squaredW = np.ndarray(W.shape)
+            for i in xrange(inp_dim):
+                for j in xrange(out_dim):
+                    orig_value = W[i][j]
+                    self.linear_layers[layer].params[0][i][j] \
+                        += epsilon
+                    self.forward(inp_tensor)
+                    E_plus = self.cross_entropy_loss(target)
+                    self.linear_layers[layer].params[0][i][j] \
+                        -= 2*epsilon
+                    self.forward(inp_tensor)
+                    E_minus = self.cross_entropy_loss(target)
+                    grad_val = (E_plus - E_minus) / (2*epsilon)
+                    calculate_W_grad[i][j] = grad_val
+                    squaredW[i][j] \
+                        = (grad_val - l1.gradParams[0][i][j])**2
+                    self.linear_layers[layer].params[0][i][j] = orig_value
+
+                    print grad_val
+                    print orig_value
+                    if not orig_value == 0.0:
+                        print abs(grad_val - orig_value)/abs(orig_value)
+                    print '--------'
+
+        # get_conv_grad()
+        get_lin_grad(layer=2)
 
     def updateParams(self, hyperParams):
         '''
@@ -92,14 +179,14 @@ class LeNet(object):
              beta2 : hyperParams[2]
         '''
         for l in self.layers:
-            if not l.name == 'maxpool':
+            if l.name not in ['maxpool']:
                 l.updateParams(hyperParams)
 
     def plotPerformance(self, filename):
         def get_pts(filename):
             with open(filename, 'r') as f:
                 data = f.read()
-            all_pts = data.split('\\n')
+            all_pts = data.split('\n')
             all_pts = all_pts[:-1]  # ignoring last \n
             all_pts = [a.split(',') for a in all_pts]
             all_pts = [[float(b) for b in a] for a in all_pts]
@@ -107,7 +194,7 @@ class LeNet(object):
             all_time = []
             all_acc = []
             all_loss = []
-            for loss, acc, t, _ in all_pts:
+            for loss, acc, t in all_pts:
                 all_loss += [loss]
                 all_acc += [acc]
                 all_time += [t]
@@ -132,24 +219,48 @@ class LeNet(object):
         plt.legend(['train loss', 'validation loss'], loc=1)
         plt.show()
 
-        plt.plot(tr_time, smoothen(tr_acc, 0.05))
-        plt.plot(val_time, smoothen(val_acc, 0.05))
+        plt.plot(tr_time, smoothen(tr_acc, 1))
+        plt.plot(val_time, smoothen(val_acc, 1))
         plt.xlabel('time')
-        plt.ylabel('loss')
-        plt.legend(['train loss', 'validation loss'], loc=1)
+        plt.ylabel('accuracy')
+        plt.legend(['train accuracy', 'validation accuracy'], loc=3)
         plt.show()
+
+    def plot_tsne(self, X, y):
+        X = np.asarray(self.get_activations(X), dtype=np.float64)
+        X_2d = bh_sne(X[0])
+        scatter(X_2d[:, 0], X_2d[:, 1], c=y)
+        show()
+
+    def get_activations(self, X):
+        output = np.ndarray(X.shape[0], 84)
+        for i in xrange(X.shape[0]):
+            output[i] = self.get_feature_vec(X[i])
+        return output
+
+    def get_feature_vec(self, image_tensor):
+        assert image_tensor.shape == (1, 28, 28)
+        out = image_tensor
+        for l in self.conv_layers:
+            out = l.forward(out)
+
+        out = out.reshape(16*4*4)
+        for l in self.layers:
+            if l.name is not 'softmax':
+                out = l.forward(out)
+            return out
 
     def test(self, test_data, test_labels):
         hits = 0
-        testsize = test_labels.shape[0]
-        # testsize = 100
+        # testsize = test_labels.shape[0]
+        testsize = 100
         for inp, target in zip(test_data, test_labels):
             self.forward(inp.reshape(1, 28, 28))
             self.cross_entropy_loss(target)
             hits += self.hit
         return (100.0*hits)/testsize
 
-    def train(self, train_data, train_labels, val_data, val_labels, config):
+    def train(self, train_data, train_labels, val_data, val_labels, config):  #
         '''
         config : a dictionary with following entries
          filename   : name of file to store results
@@ -164,10 +275,10 @@ class LeNet(object):
              beta1 : hyperParams[1]
              beta2 : hyperParams[2]
         '''
-        train_size = train_labels.shape[0]
-        val_size = val_labels.shape[0]
-        # train_size = 1600
-        # val_size = 800
+        # train_size = train_labels.shape[0]
+        # val_size = val_labels.shape[0]
+        train_size = 1600
+        val_size = 800
         shuffle_train = np.random.permutation(np.arange(0, train_size))
         shuffle_val = np.random.permutation(np.arange(0, val_size))
 
@@ -193,22 +304,17 @@ class LeNet(object):
             loss_train = 0
             hits = 0
             for i in tqdm(xrange(0, train_size, minibatch_size)):
+                mini_hits = 0
                 local_loss = 0
                 for j in xrange(0, minibatch_size):
                     index = shuffle_train[i + j]
                     self.forward(train_data[index].reshape(1, 28, 28))
                     local_loss += self.cross_entropy_loss(train_labels[index])
                     hits += self.hit
+                    mini_hits += self.hit
                     self.backward()
-
-                with open('tr_' + filename, 'a') as f:
-                    f.write(str(local_loss/minibatch_size) + ', '
-                            + str(hits*100.0/i) + ', '
-                            + str(time.time() - start_time) + ', 0\n')
-
+                print mini_hits*100.0/minibatch_size
                 self.updateParams(hyperParams)
-                if i > 7*minibatch_size:
-                    exit()
                 loss_train += local_loss
             return [loss_train/train_size, (hits*100.0)/train_size]
 
@@ -219,36 +325,34 @@ class LeNet(object):
                 self.forward(val_data[shuffle_val[i]].reshape(1, 28, 28))
                 loss_val += self.cross_entropy_loss(val_labels[shuffle_val[i]])
                 hits += self.hit
-                if i % 100 == 0 and i > 0:
-                    with open('val_' + filename, 'a') as f:
-                        f.write(str(loss_val/500) + ', '
-                                + str(hits*100.0/500) + ', '
-                                + str(time.time() - start_time) + ', 0\n')
             return [loss_val/val_size, (hits*100.0)/val_size]
 
         for epoch in xrange(0, max_epochs):
+            # shuffle_train = np.random.permutation(np.arange(0, train_size))
+            # shuffle_val = np.random.permutation(np.arange(0, val_size))
             loss_train, accuracy_train = TrainStep()
             with open('tr_' + filename, 'a') as f:
                 f.write(str(loss_train) + ', '
                         + str(accuracy_train) + ', '
-                        + str(time.time() - start_time) + ', 1\n')
+                        + str(time.time() - start_time) + '\n')
 
             loss_val, accuracy_val = ValidationStep()
             with open('val_' + filename, 'a') as f:
-                f.write(str(loss_val) + ', ' +
+                f.write(str(loss_val) + ', '
                         + str(accuracy_val) + ', '
-                        + str(time.time() - start_time) + ', 1\n')
-
-        self.plotPerformance(filename)
+                        + str(time.time() - start_time) + '\n')
 
 
 if __name__ == '__main__':
-    inp_tensor = np.zeros((1, 28, 28))
+    inp_tensor = np.random.normal(size=(1, 28, 28))
     net = LeNet()
-    net.forward(inp_tensor)
-    net.cross_entropy_loss(4)
-    net.backward(7)
-    net.backward(1)
-    net.backward(4)
-    net.backward(9)
-    net.updateParams([1, 0.9, 0.999])
+    # net.tell_time_taken()
+    net.plotPerformance('relu.log')
+    # net.forward(inp_tensor)
+    # net.cross_entropy_loss(4)
+    # net.backward(7)
+    # net.backward(1)
+    # net.backward(4)
+    # net.backward(9)
+    # net.updateParams([1, 0.9, 0.999])
+    # net.get_numeric_grads()

conv.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy import signal
 import time
+import pylab
+from PIL import Image
 
 
 class Conv(object):
@@ -15,9 +17,10 @@ class Conv(object):
                              size=(n_output_channels,
                                    n_input_channels,
                                    kr_size, kr_size))
-        b = np.random.normal(loc=0.001,
-                             scale=0.0001,
-                             size=(n_output_channels))
+        # b = np.random.normal(loc=0.001,
+        #                      scale=0.0001,
+        #                      size=(n_output_channels))
+        b = np.zeros((n_output_channels,))
         dE_dW = np.ndarray(W.shape)
         dE_db = np.ndarray(b.shape)
         self.name = 'conv'
@@ -95,16 +98,17 @@ class Conv(object):
             return np.flipud(np.fliplr(kernel))
 
         # print np.sum(dE_dW), 'c+++', np.sum(dE_db)
-        for i in xrange(self.n_output_channels):
-            gdY = np.multiply(deltas[i], self.deriv_out[i])
-            for j in xrange(self.n_input_channels):
-                dE_dX[j] += signal.convolve2d(deltas[i], flip_kr(W[i][j]))
+        for out_ch in xrange(self.n_output_channels):
+            gdY = np.multiply(deltas[out_ch], self.deriv_out[out_ch])
+            for in_ch in xrange(self.n_input_channels):
+                dE_dX[in_ch] += signal.convolve2d(deltas[out_ch],
+                                                  flip_kr(W[out_ch][in_ch]))
                 # Calculating dE_dW
-                dE_dW[i][j] = flip_kr(signal.convolve2d(self.in_fmap[j],
-                                                        flip_kr(gdY),
-                                                        mode='valid'))
+                dE_dW[out_ch][in_ch] = flip_kr(signal.convolve2d(self.in_fmap[in_ch],
+                                                                 flip_kr(gdY),
+                                                                 mode='valid'))
             # Calculating dE_db
-            dE_db[i] = np.sum(gdY)
+            dE_db[out_ch] = np.sum(gdY)
 
         # print np.sum(dE_dW), 'c---', np.sum(dE_db)
         self.gradParams = [dE_dW, dE_db]
@@ -141,10 +145,6 @@ class Conv(object):
 
         m_t = map(lambda x: x/(1 - beta1**self.i_t), self.momentum1)
         v_t = map(lambda x: x/(1 - beta2**self.i_t), self.momentum2)
-        print np.mean(self.params[0]), 'wwwwwwww'
-        print np.mean(self.gradParams[0]), 'dddddddddddd'
-        print np.mean(v_t[0]), 'vvvvvvvvvvvv'
-        print '============='
 
         self.params = map(lambda theta, m, v:
                           np.subtract(theta,
@@ -154,17 +154,33 @@ class Conv(object):
 
 
 if __name__ == '__main__':
-    n_input_channels = 1
-    n_output_channels = 6
-    c = Conv(n_input_channels, n_output_channels, 5)
-    sample_deltas = np.ndarray((n_output_channels))
-    input_map = np.ones((n_input_channels, 28, 28))
-    output = c.forward(input_map)
-    # print output.shape
-    sample_deltas = np.ones(output.shape)
-    dE_dX = c.backward(sample_deltas * 10e-7)
-    dE_dX = c.backward(sample_deltas * 10e-3)
-    dE_dX = c.backward(sample_deltas * 10e-2)
-    dE_dX = c.backward(sample_deltas * 10e-7)
-    # print np.sum(dE_dX)
-    c.updateParams([0.01, 0.9, 0.99])
+    n_input_channels = 3
+    n_output_channels = 3
+    c = Conv(n_input_channels, n_output_channels, 3)
+    # sample_deltas = np.ndarray((n_output_channels))
+    # input_map = np.ones((n_input_channels, 28, 28))
+    # output = c.forward(input_map)
+    # # print output.shape
+    # sample_deltas = np.ones(output.shape)
+    # dE_dX = c.backward(sample_deltas * 10e-7)
+    # dE_dX = c.backward(sample_deltas * 10e-3)
+    # dE_dX = c.backward(sample_deltas * 10e-2)
+    # dE_dX = c.backward(sample_deltas * 10e-7)
+    # # print np.sum(dE_dX)
+    # c.updateParams([0.01, 0.9, 0.99])
+    img = Image.open(open('3wolfmoon.jpg'))
+    # dimensions are (height, width, channel)
+    img = np.asarray(img, dtype='float64') / 256.
+
+    # put image in 4D tensor of shape (1, 3, height, width)
+    img_ = img.transpose(2, 0, 1).reshape(3, 639, 516)
+    filtered_img = c.forward(img_)
+
+    # plot original image and first and second components of output
+    pylab.subplot(1, 3, 1); pylab.axis('off'); pylab.imshow(img)
+    pylab.gray();
+    # recall that the convOp output (filtered image) is actually a "minibatch",
+    # of size 1 here, so we take index 0 in the first dimension:
+    pylab.subplot(1, 3, 2); pylab.axis('off'); pylab.imshow(filtered_img[0, :, :])
+    pylab.subplot(1, 3, 3); pylab.axis('off'); pylab.imshow(filtered_img[1, :, :])
+    pylab.show()

linear.py
@@ -105,7 +105,7 @@ class Linear(object):
                                                      (1 - beta2) *
                                                      np.square(g)),
                                  self.momentum2, self.gradParams)
-            # print np.sum(self.momentum1[0]), np.sum(self.gradParams[0])
+            print np.mean(self.momentum1[0]), np.mean(self.gradParams[0])
 
         m_t = map(lambda x: x/(1 - beta1**self.i_t), self.momentum1)
         v_t = map(lambda x: x/(1 - beta2**self.i_t), self.momentum2)

pool.py
@@ -1,4 +1,6 @@
 import numpy as np
+import time
+
 
 class MaxPool_2(object):
     def __init__(self):
@@ -9,6 +11,7 @@ class MaxPool_2(object):
         self.gradParams = []
 
     def forward(self, fmap):
+        start_time = time.time()
         n_channels, width, height = fmap.shape
         # max_idxs = np.ndarray(n_channels,
         #                       width/2,
@@ -30,6 +33,7 @@ class MaxPool_2(object):
                     mask[ch][2*row + max_idx/2 ][2*col + max_idx%2] = 1
 
         self.mask = mask
+        self.time_taken = time.time() - start_time
         return output
 
     def backward(self, deltas):
@@ -47,10 +51,11 @@ class MaxPool_2(object):
 
         return dE_dX
 
+
 if __name__ == '__main__':
-    shape = ( 4, 32, 32 )
-    a = np.ones(shape)
+    shape = (1, 4, 4)
+    a = np.arange(16).reshape(1, 4, 4)
     d = np.ones((shape[0], shape[1]/2, shape[2]/2))
     m = MaxPool_2()
-    print m.forward(a).shape
-    print np.sum(m.backward(d))
+    print m.forward(a)
+    print m.backward(d*2)

softmax.py
@@ -27,9 +27,9 @@ class Softmax(object):
             x = x - max_val
             return np.exp(x) / np.sum(np.exp(x), axis=0)
 
-        def softmax_derivative(vector):
-            return np.diag(vector) - np.dot(np.expand_dims(vector, 1),
-                                            np.expand_dims(vector, 0))
+        def softmax_derivative(probs):
+            return np.diag(probs) - np.dot(np.expand_dims(probs, 1),
+                                           np.expand_dims(probs, 0))
         self.activation_fn = softmax
         self.activation_derivative = softmax_derivative
 
@@ -42,7 +42,7 @@ class Softmax(object):
         W, b = self.params
         affine_transform = np.dot(feature_vec, W) + b
         probs = self.activation_fn(affine_transform)
-        self.deriv_out = self.activation_derivative(affine_transform)
+        self.deriv_out = self.activation_derivative(probs)
         # print np.sum(W), np.sum(b)
         self.time_taken = time.time() - start_time
         return probs